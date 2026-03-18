eee4fdc00531bb5be64146d50b49aacc7843631f
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-18 21:56:35 +0545

    refactor: Transfer logic to the goal controller and goal model cleanup

--- fitness_tracker_app/lib/models/goal_model.dart
+++ fitness_tracker_app/lib/models/goal_model.dart
@@ -1,5 +1,3 @@
-import 'package:fitness_tracker_app/models/workout_model.dart';
-
 enum GoalType { duration, distance, calorie }
 
 class GoalModel {
@@ -8,22 +6,4 @@ class GoalModel {
   final num target;
 
   GoalModel(this.title, this.type, this.target);
-
-  double calculateProgressFraction(
-    List<WorkoutModel> workouts,
-  ) {
-    double totalValue = workouts.fold(0, (sum, workout) {
-      return sum +
-          switch (type) {
-            GoalType.duration =>
-              workout.duration.toDouble(),
-            GoalType.distance =>
-              workout.distanceCovered ?? 0.0,
-            GoalType.calorie => workout.caloriesBurnt,
-          };
-    });
-
-    if (target < 0) return 0.0;
-    return (totalValue / target).clamp(0.0, 1.0);
-  }
 }
