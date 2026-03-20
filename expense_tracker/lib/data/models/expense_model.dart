ccbc996ab0ab4882b13a265476d661eaeded4d7a
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-20 08:48:11 +0545

    refactor: add new field called expenseId in expense model

--- expense_tracker/lib/data/models/expense_model.dart
+++ expense_tracker/lib/data/models/expense_model.dart
@@ -1,16 +1,20 @@
 class ExpenseModel {
+  final int _expenseId;
   final String _title;
   final String? _description;
   final double _expenseAmount;
 
   ExpenseModel({
+    required expenseId,
     required title,
     description,
     required expenseAmount,
-  }) : _title = title,
+  }) : _expenseId = expenseId,
+       _title = title,
        _description = description,
        _expenseAmount = expenseAmount;
 
+  int get expenseId => _expenseId;
   String get title => _title;
   String? get description => _description;
   double get expenseAmount => _expenseAmount;
