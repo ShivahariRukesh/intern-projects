2c43c7ac92e5a408083f404d3aafd91807ab123f
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-23 10:18:16 +0545

    refactor: use custom input field widget
    Instead of using repeated huge lines of code for same functionality in ma
    ny parts of the file, custom input field widget was reused

--- fitness_tracker_app/lib/views/workout_log/widgets/workout_form_widget.dart
+++ fitness_tracker_app/lib/views/workout_log/widgets/workout_form_widget.dart
@@ -1,8 +1,8 @@
 import 'package:fitness_tracker_app/models/workout_model.dart';
 import 'package:fitness_tracker_app/utils/global_instance.dart';
+import 'package:fitness_tracker_app/widgets/shared/custom_input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fitness_tracker_app/services/fitness_manager.dart';
 
 class WorkoutFormWidget extends StatefulWidget {
   const WorkoutFormWidget({super.key});
@@ -33,6 +33,7 @@ class _WorkoutFormWidgetState
       _durationError = null;
       _calorieError = null;
       _workoutError = null;
+      _distanceError = null;
 
       // Require workout
       if (_workoutTypeController.text.isEmpty) {
@@ -49,10 +50,21 @@ class _WorkoutFormWidgetState
         _calorieError = "Calories are required";
       }
 
+      // Require distance
+      if (_distanceTextController.text.isEmpty) {
+        _distanceError = "Distance is required";
+      }
+
+      if (!(_workoutTypeController.text == "CLIMBING" ||
+          _workoutTypeController.text == "JOGGING")) {
+        _distanceError = null;
+      }
+
       // If all required fields are filled
       if (_workoutError == null &&
           _durationError == null &&
-          _calorieError == null) {
+          _calorieError == null &&
+          _distanceError == null) {
         // fitnessService.addWorkout(
         //   _workoutTypeController.text.toLowerCase(),
         //   int.parse(_durationTextController.text),
@@ -66,7 +78,17 @@ class _WorkoutFormWidgetState
           _workoutTypeController.text.toLowerCase() ==
                   "pullup"
               ? WorkoutType.pullup
-              : WorkoutType.jogging,
+              : _workoutTypeController.text.toLowerCase() ==
+                    "jogging"
+              ? WorkoutType.jogging
+              : _workoutTypeController.text.toLowerCase() ==
+                    "situp"
+              ? WorkoutType.situp
+              : _workoutTypeController.text.toLowerCase() ==
+                    "plank"
+              ? WorkoutType.plank
+              : WorkoutType.climbing,
+
           int.parse(_durationTextController.text),
           double.parse(_calorieTextController.text),
           _distanceTextController.text.isEmpty
@@ -99,167 +121,319 @@ class _WorkoutFormWidgetState
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: 400,
+    debugPrint("yo ${_workoutTypeController.text}");
+    return SingleChildScrollView(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Fields marked with (*) are meant to be required",
-                style: TextStyle(
-                  color: const Color.fromARGB(
-                    255,
-                    135,
-                    55,
-                    149,
-                  ),
+      child: Center(
+        child: Card(
+          elevation: 6,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.stretch,
+              children: [
+                // Subtitle
+                Text(
+                  "Fields marked with (*) are required",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodySmall,
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              DropdownMenu(
-                width: 250,
-                controller: _workoutTypeController,
-                hintText: "Select Your Workout *",
-                onSelected: (String? value) =>
-                    _workoutTypeController.text =
-                        value ?? "",
-                dropdownMenuEntries: WorkoutType.values
-                    .map(
-                      (workout) => DropdownMenuEntry(
-                        value: workout.name.toUpperCase(),
-                        label: workout.name.toUpperCase(),
+
+                const SizedBox(height: 20),
+
+                // Workout Type Dropdown
+                DropdownMenu(
+                  textStyle: Theme.of(
+                    context,
+                  ).textTheme.displayMedium,
+                  leadingIcon: Icon(Icons.fitness_center),
+                  width: double.infinity,
+                  controller: _workoutTypeController,
+                  hintText: "Select Your Workout *",
+                  onSelected: (String? value) {
+                    setState(() {
+                      _workoutTypeController.text =
+                          value ?? "";
+                    });
+                  },
+                  dropdownMenuEntries: WorkoutType.values
+                      .map(
+                        (workout) => DropdownMenuEntry(
+                          value: workout.name.toUpperCase(),
+                          label: workout.name.toUpperCase(),
+                        ),
+                      )
+                      .toList(),
+                ),
+                if (_workoutError != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Text(
+                      _workoutError!,
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 12,
                       ),
-                    )
-                    .toList(),
-              ),
-              if (_workoutError != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    _workoutError!,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
                     ),
                   ),
-                ),
-            ],
-          ),
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _durationTextController,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.timer_outlined),
-                  labelText: "Enter the duration *",
+                const SizedBox(height: 20),
+
+                // Duration Input
+                CustomInputField(
+                  controller: _durationTextController,
+                  label: "Enter the duration *",
+                  icon: Icons.timer_outlined,
                   suffixText: "minutes",
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  errorText: _durationError,
                 ),
-              ),
-              if (_durationError != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    _durationError!,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-            ],
-          ),
+                const SizedBox(height: 20),
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _distanceTextController,
-                keyboardType:
-                    const TextInputType.numberWithOptions(
-                      decimal: true,
-                    ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(
-                    RegExp(r'^\d*\.?\d*'),
+                // Distance Input
+                if (_workoutTypeController.text ==
+                        "CLIMBING" ||
+                    _workoutTypeController.text ==
+                        "JOGGING") ...[
+                  CustomInputField(
+                    controller: _distanceTextController,
+                    label: "Enter the distance *",
+                    icon: Icons.straighten_outlined,
+                    suffixText: "km",
+                    keyboardType:
+                        const TextInputType.numberWithOptions(
+                          decimal: true,
+                        ),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'^\d*\.?\d*'),
+                      ),
+                    ],
+                    errorText: _distanceError,
                   ),
+                  const SizedBox(height: 20),
                 ],
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.straighten_outlined,
-                  ),
-                  labelText: "Enter the distance",
-                  suffixText: "km",
-                ),
-              ),
-              if (_distanceError != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    _distanceError!,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-            ],
-          ),
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _calorieTextController,
-                keyboardType:
-                    const TextInputType.numberWithOptions(
-                      decimal: true,
-                    ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(
-                    RegExp(r'^\d*\.?\d*'),
-                  ),
-                ],
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.local_fire_department_outlined,
-                  ),
-                  labelText: "Enter the calorie *",
+                // Calorie Input
+                CustomInputField(
+                  controller: _calorieTextController,
+                  label: "Enter the calorie *",
+                  icon:
+                      Icons.local_fire_department_outlined,
                   suffixText: "kCal",
+                  keyboardType:
+                      const TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'^\d*\.?\d*'),
+                    ),
+                  ],
+                  errorText: _calorieError,
                 ),
-              ),
-              if (_calorieError != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    _calorieError!,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
+                const SizedBox(height: 30),
+
+                // Submit Button
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: submitWorkoutForm,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "Submit",
+                      style: Theme.of(
+                        context,
+                      ).textTheme.bodyMedium,
                     ),
                   ),
                 ),
-            ],
-          ),
-
-          Center(
-            child: ElevatedButton(
-              onPressed: submitWorkoutForm,
-              child: const Text("Submit"),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
+
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     height: 400,
+  //     width: 400,
+  //     padding: const EdgeInsets.all(16),
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //       children: [
+  //         Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Text(
+  //               "Fields marked with (*) are meant to be required",
+  //               style: TextStyle(
+  //                 color: const Color.fromARGB(
+  //                   255,
+  //                   135,
+  //                   55,
+  //                   149,
+  //                 ),
+  //               ),
+  //             ),
+  //             DropdownMenu(
+  //               width: 250,
+  //               controller: _workoutTypeController,
+  //               hintText: "Select Your Workout *",
+  //               onSelected: (String? value) =>
+  //                   _workoutTypeController.text =
+  //                       value ?? "",
+  //               dropdownMenuEntries: WorkoutType.values
+  //                   .map(
+  //                     (workout) => DropdownMenuEntry(
+  //                       value: workout.name.toUpperCase(),
+  //                       label: workout.name.toUpperCase(),
+  //                     ),
+  //                   )
+  //                   .toList(),
+  //             ),
+  //             if (_workoutError != null)
+  //               Padding(
+  //                 padding: const EdgeInsets.only(top: 4),
+  //                 child: Text(
+  //                   _workoutError!,
+  //                   style: const TextStyle(
+  //                     color: Colors.red,
+  //                     fontSize: 12,
+  //                   ),
+  //                 ),
+  //               ),
+  //           ],
+  //         ),
+
+  //         Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             TextField(
+  //               controller: _durationTextController,
+  //               keyboardType: TextInputType.number,
+  //               inputFormatters: [
+  //                 FilteringTextInputFormatter.digitsOnly,
+  //               ],
+  //               decoration: const InputDecoration(
+  //                 prefixIcon: Icon(Icons.timer_outlined),
+  //                 labelText: "Enter the duration *",
+  //                 suffixText: "minutes",
+  //               ),
+  //             ),
+  //             if (_durationError != null)
+  //               Padding(
+  //                 padding: const EdgeInsets.only(top: 4),
+  //                 child: Text(
+  //                   _durationError!,
+  //                   style: const TextStyle(
+  //                     color: Colors.red,
+  //                     fontSize: 12,
+  //                   ),
+  //                 ),
+  //               ),
+  //           ],
+  //         ),
+
+  //         Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             TextField(
+  //               controller: _distanceTextController,
+  //               keyboardType:
+  //                   const TextInputType.numberWithOptions(
+  //                     decimal: true,
+  //                   ),
+  //               inputFormatters: [
+  //                 FilteringTextInputFormatter.allow(
+  //                   RegExp(r'^\d*\.?\d*'),
+  //                 ),
+  //               ],
+  //               decoration: const InputDecoration(
+  //                 prefixIcon: Icon(
+  //                   Icons.straighten_outlined,
+  //                 ),
+  //                 labelText: "Enter the distance",
+  //                 suffixText: "km",
+  //               ),
+  //             ),
+  //             if (_distanceError != null)
+  //               Padding(
+  //                 padding: const EdgeInsets.only(top: 4),
+  //                 child: Text(
+  //                   _distanceError!,
+  //                   style: const TextStyle(
+  //                     color: Colors.red,
+  //                     fontSize: 12,
+  //                   ),
+  //                 ),
+  //               ),
+  //           ],
+  //         ),
+
+  //         Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             TextField(
+  //               controller: _calorieTextController,
+  //               keyboardType:
+  //                   const TextInputType.numberWithOptions(
+  //                     decimal: true,
+  //                   ),
+  //               inputFormatters: [
+  //                 FilteringTextInputFormatter.allow(
+  //                   RegExp(r'^\d*\.?\d*'),
+  //                 ),
+  //               ],
+  //               decoration: const InputDecoration(
+  //                 prefixIcon: Icon(
+  //                   Icons.local_fire_department_outlined,
+  //                 ),
+  //                 labelText: "Enter the calorie *",
+  //                 suffixText: "kCal",
+  //               ),
+  //             ),
+  //             if (_calorieError != null)
+  //               Padding(
+  //                 padding: const EdgeInsets.only(top: 4),
+  //                 child: Text(
+  //                   _calorieError!,
+  //                   style: const TextStyle(
+  //                     color: Colors.red,
+  //                     fontSize: 12,
+  //                   ),
+  //                 ),
+  //               ),
+  //           ],
+  //         ),
+
+  //         Center(
+  //           child: ElevatedButton(
+  //             onPressed: submitWorkoutForm,
+  //             child: const Text("Submit"),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
