66f4c09fbfcda0a170ebb86018413a8608b65753
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-23 11:26:15 +0545

    refactor: implement flutter lint rule that specify data types

--- fitness_tracker_app/lib/services/fitness_manager.dart
+++ fitness_tracker_app/lib/services/fitness_manager.dart
@@ -2,12 +2,13 @@ import '../models/workout_model.dart';
 import '../models/goal_model.dart';
 
 class FitnessManager {
-  final List<WorkoutModel> _workouts = [];
-  final List<GoalModel> _goals = [];
+  final List<WorkoutModel> _workouts = <WorkoutModel>[];
+  final List<GoalModel> _goals = <GoalModel>[];
 
   List<WorkoutModel> get workouts =>
-      List.unmodifiable(_workouts);
-  List<GoalModel> get goals => List.unmodifiable(_goals);
+      List<WorkoutModel>.unmodifiable(_workouts);
+  List<GoalModel> get goals =>
+      List<GoalModel>.unmodifiable(_goals);
 
   void addWorkout(WorkoutModel workout) {
     _workouts.add(workout);
