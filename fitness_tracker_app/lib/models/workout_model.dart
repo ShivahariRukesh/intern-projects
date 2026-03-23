383c6d55e1dc34a65db4220cca0c7c5eb1903963
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-23 10:01:21 +0545

    feat: add new workout sessions that extends Workout class

--- fitness_tracker_app/lib/models/workout_model.dart
+++ fitness_tracker_app/lib/models/workout_model.dart
@@ -1,4 +1,4 @@
-enum WorkoutType { jogging, pullup }
+enum WorkoutType { jogging, pullup, situp, plank, climbing }
 
 abstract class WorkoutModel {
   final int duration;
@@ -31,6 +31,25 @@ class Jogging extends WorkoutModel {
   String toString() => "Jogging";
 }
 
+class Climbing extends WorkoutModel {
+  final double distance;
+
+  Climbing({
+    required int duration,
+    required double caloriesBurnt,
+    required this.distance,
+  }) : super(
+         duration: duration,
+         caloriesBurnt: caloriesBurnt,
+       );
+
+  @override
+  double getDistance() => distance;
+
+  @override
+  String toString() => "Climbing";
+}
+
 class Pullup extends WorkoutModel {
   Pullup({
     required int duration,
@@ -42,4 +61,30 @@ class Pullup extends WorkoutModel {
 
   @override
   String toString() => "Pullup";
+}
+
+class Situp extends WorkoutModel {
+  Situp({
+    required int duration,
+    required double caloriesBurnt,
+  }) : super(
+         duration: duration,
+         caloriesBurnt: caloriesBurnt,
+       );
+
+  @override
+  String toString() => "Situp";
+}
+
+class Plank extends WorkoutModel {
+  Plank({
+    required int duration,
+    required double caloriesBurnt,
+  }) : super(
+         duration: duration,
+         caloriesBurnt: caloriesBurnt,
+       );
+
+  @override
+  String toString() => "Plank";
 }
