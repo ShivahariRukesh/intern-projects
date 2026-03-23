21a5c20db01cb34aa86ed437ef98378f16f81547
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-23 10:54:47 +0545

    refactor: implement flutter linter rules
    Applied the rules like specify data types and prefer single quotes

--- fitness_tracker_app/lib/widgets/shared/scaffold_widget.dart
+++ fitness_tracker_app/lib/widgets/shared/scaffold_widget.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 
-List routes = [
-  {
-    "name": 'Dashboard',
-    "route": '/dashboard',
-    "icon": Icon(Icons.grid_view_rounded),
+List<Map<String, Object>> routes = <Map<String, Object>>[
+  <String, Object>{
+    'name': 'Dashboard',
+    'route': '/dashboard',
+    'icon': const Icon(Icons.grid_view_rounded),
   },
-  {
-    "name": 'Log',
-    "route": '/log',
-    "icon": Icon(Icons.add_box_outlined),
+  <String, Object>{
+    'name': 'Log',
+    'route': '/log',
+    'icon': const Icon(Icons.add_box_outlined),
   },
-  {
-    "name": 'History',
-    "route": '/history',
-    "icon": Icon(Icons.history),
+  <String, Object>{
+    'name': 'History',
+    'route': '/history',
+    'icon': const Icon(Icons.history),
   },
 
-  {
-    "name": 'Goal',
-    "route": '/goal',
-    "icon": Icon(Icons.ballot),
+  <String, Object>{
+    'name': 'Goal',
+    'route': '/goal',
+    'icon': const Icon(Icons.ballot),
   },
 ];
 
@@ -50,31 +50,32 @@ class ScaffoldWidget extends StatelessWidget {
           ),
         ),
         title: Text(title),
-        actions: actions ?? [],
+        actions: actions ?? <Widget>[],
         elevation: 3,
       ),
       body: body,
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: routes.indexWhere(
-          (route) =>
-              route["route"] ==
+          (Map<String, Object> route) =>
+              route['route'] ==
               ModalRoute.of(context)!.settings.name,
         ),
 
-        onTap: (value) {
+        onTap: (int value) {
           Navigator.pushReplacementNamed(
             context,
-            routes[value]["route"],
+            routes[value]['route'] as String,
           );
         },
 
         items: routes
             .map(
-              (route) => BottomNavigationBarItem(
-                icon: route["icon"],
-                label: route["name"],
-              ),
+              (Map<String, Object> route) =>
+                  BottomNavigationBarItem(
+                    icon: route['icon'] as Icon,
+                    label: route['name'] as String,
+                  ),
             )
             .toList(),
       ),
