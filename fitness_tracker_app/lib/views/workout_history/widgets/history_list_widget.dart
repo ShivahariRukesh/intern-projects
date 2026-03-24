2ae0f9c6762a4bb9c0acd13717361a068a80e6fa
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-24 14:53:14 +0545

    refactor: wrap widgets with method and added documentation comment in workout_history widgets

--- fitness_tracker_app/lib/views/workout_history/widgets/history_list_widget.dart
+++ fitness_tracker_app/lib/views/workout_history/widgets/history_list_widget.dart
@@ -2,6 +2,9 @@ import 'package:fitness_tracker_app/models/workout_model.dart';
 import 'package:fitness_tracker_app/utils/global_instance.dart';
 import 'package:flutter/material.dart';
 
+/// Lists all the workout completed
+///
+/// Keeps the track of history of workouts which was submitted using the workout form.
 class HistoryListWidget extends StatelessWidget {
   const HistoryListWidget({super.key});
 
@@ -13,14 +16,12 @@ class HistoryListWidget extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: ListView.separated(
         itemCount: fitnessService.workouts.length,
-
         separatorBuilder:
             (BuildContext context, int index) =>
                 const SizedBox(height: 12),
         itemBuilder: (BuildContext context, int index) {
           final WorkoutModel workout =
               fitnessService.workouts[index];
-
           return Container(
             decoration: BoxDecoration(
               color: theme.listTileTheme.tileColor,
@@ -51,15 +52,12 @@ class HistoryListWidget extends StatelessWidget {
                       size: 18,
                     ),
                     const SizedBox(width: 4),
-
                     Text(
                       '${workout.caloriesBurnt}\t',
                       style: theme.textTheme.displayMedium,
                     ),
                     const Text('kcal'),
-
                     const SizedBox(width: 20),
-
                     const Icon(
                       Icons.timer_outlined,
                       size: 18,
@@ -70,35 +68,8 @@ class HistoryListWidget extends StatelessWidget {
                       style: theme.textTheme.displayMedium,
                     ),
                     const Text('min'),
-
                     const SizedBox(width: 20),
-
-                    if (workout is Jogging) ...<Widget>[
-                      const Icon(
-                        Icons.straighten_outlined,
-                        size: 18,
-                      ),
-                      const SizedBox(width: 4),
-                      Text(
-                        '${workout.distance}\t',
-                        style:
-                            theme.textTheme.displayMedium,
-                      ),
-                      const Text('km'),
-                    ] else if (workout
-                        is Climbing) ...<Widget>[
-                      const Icon(
-                        Icons.straighten_outlined,
-                        size: 18,
-                      ),
-                      const SizedBox(width: 4),
-                      Text(
-                        '${workout.distance}\t',
-                        style:
-                            theme.textTheme.displayMedium,
-                      ),
-                      const Text('km'),
-                    ],
+                    ..._showDistanceMeasure(workout, theme),
                   ],
                 ),
               ),
@@ -107,5 +78,34 @@ class HistoryListWidget extends StatelessWidget {
         },
       ),
     );
+  }
+
+  List<Widget> _showDistanceMeasure(
+    WorkoutModel workout,
+    ThemeData theme,
+  ) {
+    if (workout is Jogging) {
+      return <Widget>[
+        const Icon(Icons.straighten_outlined, size: 18),
+        const SizedBox(width: 4),
+        Text(
+          '${workout.distance}\t',
+          style: theme.textTheme.displayMedium,
+        ),
+        const Text('km'),
+      ];
+    } else if (workout is Climbing) {
+      return <Widget>[
+        const Icon(Icons.straighten_outlined, size: 18),
+        const SizedBox(width: 4),
+        Text(
+          '${workout.distance}\t',
+          style: theme.textTheme.displayMedium,
+        ),
+        const Text('km'),
+      ];
+    } else {
+      return <Widget>[const SizedBox()];
+    }
   }
 }
