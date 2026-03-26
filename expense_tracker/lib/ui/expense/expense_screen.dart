7115f43d7596df3d50dc62ce52e19908278a2194
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-26 08:57:07 +0545

    refactor: change ui of expense screen

--- expense_tracker/lib/ui/expense/expense_screen.dart
+++ expense_tracker/lib/ui/expense/expense_screen.dart
@@ -1,5 +1,5 @@
+import 'package:expense_tracker/ui/core/shared/base_expense_form_widget.dart';
 import 'package:expense_tracker/ui/expense/view_models/expense_view_model.dart';
-import 'package:expense_tracker/ui/expense/widgets/expense_add_bottom_sheet_widget.dart';
 import 'package:expense_tracker/ui/expense/widgets/expense_list_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -21,19 +21,49 @@ class _ExpenseScreenState extends State<ExpenseScreen> {
     });
   }
 
+  void _openAddExpense() {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      // isScrollControlled: true,
+      context: context,
+      builder: (_) => BaseExpenseFormWidget(
+        titleText: 'Add Expense',
+        buttonText: 'Save Expense',
+        onSubmit: (ctx, title, desc, amount) {
+          ctx.read<ExpenseViewModel>().createExpense(
+            title: title,
+            description: desc,
+            amount: amount,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-          color: Colors.black,
+    return Scaffold(
+      backgroundColor: const Color(
+        0xFF0F172A,
+      ), // modern dark blue
+      appBar: AppBar(
+        title: const Text(
+          'Expenses Tracker',
+          style: TextStyle(color: Colors.blueAccent),
         ),
+        backgroundColor: Colors.transparent,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openAddExpense,
+        backgroundColor: const Color(0xFF6366F1),
+        child: const Icon(Icons.add),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          spacing: 100,
           children: [
-            ExpenseListWidget(),
-            const ExpenseAddBottomSheetWidget(),
+            SizedBox(height: 10),
+            Expanded(child: ExpenseListWidget()),
           ],
         ),
       ),
