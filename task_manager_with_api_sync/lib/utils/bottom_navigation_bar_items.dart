63e2a30f36399565c0278ac417e0aa39cc458033
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-31 14:49:29 +0545

    feat: add constant values for the bottom navigation bar items

---
+++ task_manager_with_api_sync/lib/utils/bottom_navigation_bar_items.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const List<Map<String, Object>> bottomNavigationBarItems =
+    <Map<String, Object>>[
+      <String, Object>{
+        'icon': Icon(Icons.home),
+        'label': 'Home',
+      },
+      <String, Object>{
+        'icon': Icon(Icons.person),
+        'label': 'Profile',
+      },
+    ];
