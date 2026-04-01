35b7da73925c7deb96d49540f7b079a71654c501
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-01 13:40:32 +0545

    refactor: change to stateless widget

--- task_manager_with_api_sync/lib/ui/core/shared/base_bottom_bar_widget.dart
+++ task_manager_with_api_sync/lib/ui/core/shared/base_bottom_bar_widget.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:task_manager_with_api_sync/utils/bottom_navigation_bar_items.dart';
 
-class BaseBottomBarWidget extends StatefulWidget {
+class BaseBottomBarWidget extends StatelessWidget {
   const BaseBottomBarWidget({super.key});
 
   @override
-  State<BaseBottomBarWidget> createState() =>
-      _BaseBottomBarWidgetState();
-}
-
-class _BaseBottomBarWidgetState
-    extends State<BaseBottomBarWidget> {
-  int tappedItemIndex = 0;
-  @override
   Widget build(BuildContext context) {
+    final String currentUrl = GoRouterState.of(
+      context,
+    ).uri.toString();
+
     return BottomNavigationBar(
-      onTap: (int index) => setState(() {
-        tappedItemIndex = index;
-      }),
-      currentIndex: tappedItemIndex,
+      onTap: (int index) => context.go(
+        bottomNavigationBarItems[index]['path'] as String,
+      ),
+      currentIndex: bottomNavigationBarItems.indexWhere(
+        (Map<String, Object> item) =>
+            item['path'] as String == currentUrl,
+      ),
       items: bottomNavigationBarItems
           .map(
             (Map<String, Object> item) =>
