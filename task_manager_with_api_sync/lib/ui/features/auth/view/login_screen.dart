f0f8bdc69c44debbe12e726d13643885a94ebbb8
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-01 13:41:15 +0545

    refactor: change login screen and home screen layout

--- task_manager_with_api_sync/lib/ui/features/auth/view/login_screen.dart
+++ task_manager_with_api_sync/lib/ui/features/auth/view/login_screen.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:task_manager_with_api_sync/ui/core/shared/base_scaffold_widget.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.all(10),
-      decoration: const BoxDecoration(
-        color: Colors.black45,
-      ),
-      child: const Column(
-        children: <Widget>[
-          Text('Login'),
-          Text('Username'),
-          Text('Password'),
-        ],
+    return BaseScaffoldWidget(
+      title: 'Login',
+      scaffoldBody: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(10),
+
+        child: Column(
+          children: <Widget>[
+            const Text('Login'),
+            const Text('Username'),
+            const Text('Password'),
+            OutlinedButton(
+              onPressed: () => context.go('/'),
+              child: const Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- task_manager_with_api_sync/lib/ui/features/home/view/home_screen.dart
+++ task_manager_with_api_sync/lib/ui/features/home/view/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_with_api_sync/ui/core/shared/base_scaffold_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.amber),
-
-      child: const Text('Welcome to home screen'),
+    return BaseScaffoldWidget(
+      title: 'Home',
+      scaffoldBody: Container(
+        child: const Text('Welcome to home screen'),
+      ),
     );
   }
 }
