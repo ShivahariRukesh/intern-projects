ecf56f5236bd6bf3cb10595e563d97fc482b03b0
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-01 13:42:14 +0545

    refactor: move go router constants to another file

--- task_manager_with_api_sync/lib/main.dart
+++ task_manager_with_api_sync/lib/main.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:task_manager_with_api_sync/ui/features/auth/view/login_screen.dart';
-import 'package:task_manager_with_api_sync/ui/features/home/view/home_screen.dart';
+import 'package:task_manager_with_api_sync/ui/core/theme/app_theme.dart';
+import 'package:task_manager_with_api_sync/utils/go_router.dart';
 
 void main() {
   runApp(mainApp());
 }
 
 Widget mainApp() {
-  return MaterialApp.router(routerConfig: _router);
+  return MaterialApp.router(
+    debugShowCheckedModeBanner: false,
+    title: 'Task Manager',
+    routerConfig: goRouter,
+    theme: AppTheme.lightTheme,
+  );
 }
-
-final GoRouter _router = GoRouter(
-  initialLocation: '/login',
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder:
-          (BuildContext context, GoRouterState state) =>
-              const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/login',
-      builder:
-          (BuildContext context, GoRouterState state) =>
-              const LoginScreen(),
-    ),
-  ],
-);
