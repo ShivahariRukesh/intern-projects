4163024143e835b747c76523d78b06dba7f0f85d
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-02 11:17:42 +0545

    feat: add multiprovider in the root tree

--- task_manager_with_api_sync/lib/main.dart
+++ task_manager_with_api_sync/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:task_manager_with_api_sync/ui/core/theme/app_theme.dart';
+import 'package:task_manager_with_api_sync/ui/features/task/view_model/task_view_model.dart';
 import 'package:task_manager_with_api_sync/utils/go_router.dart';
 
 void main() {
-  runApp(mainApp());
+  runApp(
+    MultiProvider(
+      providers: <SingleChildWidget>[
+        ChangeNotifierProvider<TaskViewModel>(
+          create: (BuildContext context) => TaskViewModel(),
+        ),
+      ],
+      child: mainApp(),
+    ),
+  );
 }
 
 Widget mainApp() {
