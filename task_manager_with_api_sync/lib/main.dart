66bd98beae1e9ed84905b48e815539b612bbda06
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-03 12:45:03 +0545

    feat: configure dio package

--- task_manager_with_api_sync/lib/main.dart
+++ task_manager_with_api_sync/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:task_manager_with_api_sync/data/repositories/task_repository.dart';
 import 'package:task_manager_with_api_sync/data/services/database/task_database_service.dart';
+import 'package:task_manager_with_api_sync/data/services/network/task_dio_service.dart';
 import 'package:task_manager_with_api_sync/ui/core/theme/app_theme.dart';
 import 'package:task_manager_with_api_sync/ui/features/task/view_model/task_view_model.dart';
 import 'package:task_manager_with_api_sync/utils/go_router.dart';
@@ -14,10 +15,14 @@ void main() {
         Provider<TaskDatabaseService>(
           create: (_) => TaskDatabaseService(),
         ),
+        Provider<TaskDioService>(
+          create: (_) => TaskDioService(),
+        ),
         Provider<TaskRepository>(
           create: (BuildContext context) => TaskRepository(
             taskDatabaseService: context
                 .read<TaskDatabaseService>(),
+            taskDioService: context.read<TaskDioService>(),
           ),
         ),
         ChangeNotifierProvider<TaskViewModel>(
