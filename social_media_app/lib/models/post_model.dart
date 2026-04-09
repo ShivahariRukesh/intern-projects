92616c982714f768704328453740e5c3743b8cfd
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-09 08:21:18 +0545

    feat: add post model for posts features

---
+++ social_media_app/lib/models/post_model.dart
@@ -0,0 +1,29 @@
+class PostModel {
+  final int id;
+  final int userId;
+  final String title;
+  final String? imageUrl;
+  final int likes;
+  final int views;
+
+  PostModel(
+      {required this.id,
+      required this.userId,
+      required this.title,
+      required this.imageUrl,
+      required this.likes,
+      required this.views});
+
+  factory PostModel.fromJson(
+      Map<String, dynamic> filteredJsonData) {
+    return PostModel(
+        id: filteredJsonData["id"] as int,
+        userId: filteredJsonData["userId"] as int,
+        title: filteredJsonData["title"] as String,
+        imageUrl:
+            filteredJsonData["imageUrl"] as String? ?? '',
+        likes:
+            filteredJsonData["reactions"]["likes"] as int,
+        views: filteredJsonData["views"] as int);
+  }
+}
