69d1263cc81b356e80fee7a2706fd0d775f164f3
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-04-13 12:38:27 +0545

    feat: remove old story viewer widget to implment the story screen view and add stories navigation logic inside its own view model

--- social_media_app/lib/app/app.router.dart
+++ social_media_app/lib/app/app.router.dart
@@ -6,13 +6,15 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i5;
+import 'package:flutter/material.dart' as _i6;
 import 'package:flutter/material.dart';
 import 'package:social_media_app/ui/views/auth/auth_view.dart' as _i4;
 import 'package:social_media_app/ui/views/home/home_view.dart' as _i2;
 import 'package:social_media_app/ui/views/startup/startup_view.dart' as _i3;
+import 'package:social_media_app/ui/views/story_screen/story_screen_view.dart'
+    as _i5;
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i6;
+import 'package:stacked_services/stacked_services.dart' as _i7;
 
 class Routes {
   static const homeView = '/home-view';
@@ -21,10 +23,13 @@ class Routes {
 
   static const authView = '/auth-view';
 
+  static const storyScreenView = '/story-screen-view';
+
   static const all = <String>{
     homeView,
     startupView,
     authView,
+    storyScreenView,
   };
 }
 
@@ -42,6 +47,10 @@ class StackedRouter extends _i1.RouterBase {
       Routes.authView,
       page: _i4.AuthView,
     ),
+    _i1.RouteDef(
+      Routes.storyScreenView,
+      page: _i5.StoryScreenView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
@@ -49,7 +58,7 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<HomeViewArguments>(
         orElse: () => const HomeViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => _i2.HomeView(key: args.key),
         settings: data,
       );
@@ -58,7 +67,7 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<StartupViewArguments>(
         orElse: () => const StartupViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => _i3.StartupView(key: args.key),
         settings: data,
       );
@@ -67,8 +76,20 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<AuthViewArguments>(
         orElse: () => const AuthViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => _i4.AuthView(key: args.key),
+        settings: data,
+      );
+    },
+    _i5.StoryScreenView: (data) {
+      final args = data.getArgs<StoryScreenViewArguments>(nullOk: false);
+      return _i6.MaterialPageRoute<dynamic>(
+        builder: (context) => _i5.StoryScreenView(
+            key: args.key,
+            stories: args.stories,
+            initialIndex: args.initialIndex,
+            usernameMap: args.usernameMap,
+            storyList: args.storyList),
         settings: data,
       );
     },
@@ -84,7 +105,7 @@ class StackedRouter extends _i1.RouterBase {
 class HomeViewArguments {
   const HomeViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i6.Key? key;
 
   @override
   String toString() {
@@ -106,7 +127,7 @@ class HomeViewArguments {
 class StartupViewArguments {
   const StartupViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i6.Key? key;
 
   @override
   String toString() {
@@ -128,7 +149,7 @@ class StartupViewArguments {
 class AuthViewArguments {
   const AuthViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i6.Key? key;
 
   @override
   String toString() {
@@ -147,9 +168,53 @@ class AuthViewArguments {
   }
 }
 
-extension NavigatorStateExtension on _i6.NavigationService {
+class StoryScreenViewArguments {
+  const StoryScreenViewArguments({
+    this.key,
+    required this.stories,
+    required this.initialIndex,
+    required this.usernameMap,
+    required this.storyList,
+  });
+
+  final _i6.Key? key;
+
+  final List<List<String>> stories;
+
+  final int initialIndex;
+
+  final dynamic usernameMap;
+
+  final List<dynamic> storyList;
+
+  @override
+  String toString() {
+    return '{"key": "$key", "stories": "$stories", "initialIndex": "$initialIndex", "usernameMap": "$usernameMap", "storyList": "$storyList"}';
+  }
+
+  @override
+  bool operator ==(covariant StoryScreenViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key &&
+        other.stories == stories &&
+        other.initialIndex == initialIndex &&
+        other.usernameMap == usernameMap &&
+        other.storyList == storyList;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode ^
+        stories.hashCode ^
+        initialIndex.hashCode ^
+        usernameMap.hashCode ^
+        storyList.hashCode;
+  }
+}
+
+extension NavigatorStateExtension on _i7.NavigationService {
   Future<dynamic> navigateToHomeView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -165,7 +230,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> navigateToStartupView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -181,7 +246,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> navigateToAuthView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -196,8 +261,33 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToStoryScreenView({
+    _i6.Key? key,
+    required List<List<String>> stories,
+    required int initialIndex,
+    required dynamic usernameMap,
+    required List<dynamic> storyList,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.storyScreenView,
+        arguments: StoryScreenViewArguments(
+            key: key,
+            stories: stories,
+            initialIndex: initialIndex,
+            usernameMap: usernameMap,
+            storyList: storyList),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithHomeView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -213,7 +303,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> replaceWithStartupView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -229,7 +319,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> replaceWithAuthView({
-    _i5.Key? key,
+    _i6.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -238,6 +328,31 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }) async {
     return replaceWith<dynamic>(Routes.authView,
         arguments: AuthViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithStoryScreenView({
+    _i6.Key? key,
+    required List<List<String>> stories,
+    required int initialIndex,
+    required dynamic usernameMap,
+    required List<dynamic> storyList,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.storyScreenView,
+        arguments: StoryScreenViewArguments(
+            key: key,
+            stories: stories,
+            initialIndex: initialIndex,
+            usernameMap: usernameMap,
+            storyList: storyList),
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
--- social_media_app/lib/ui/views/home/widgets/story_list_widget.dart
+++ social_media_app/lib/ui/views/home/widgets/story_list_widget.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/app/app.locator.dart';
+import 'package:social_media_app/app/app.router.dart';
 import 'package:social_media_app/models/post_model.dart';
 import 'package:social_media_app/ui/views/home/home_viewmodel.dart';
 import 'package:social_media_app/utils/shuffle_list.dart';
 import 'package:stacked/stacked.dart';
-import 'story_viewer_screen.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class Story {
   final String imageUrl;
   Story({required this.imageUrl});
 }
 
-class StoryListWidget extends ViewModelWidget<HomeViewModel> {
+class StoryListWidget
+    extends ViewModelWidget<HomeViewModel> {
   const StoryListWidget({super.key});
 
   @override
-  Widget build(BuildContext context, HomeViewModel viewModel) {
+  Widget build(
+      BuildContext context, HomeViewModel viewModel) {
     final storyList = viewModel.storyList;
-    final List storyImages = List.generate(
+    final List<List<String>> storyImages = List.generate(
       storyList.length,
       (index) => [
         "https://picsum.photos/seed/${index + 100}/200/200",
@@ -32,15 +36,12 @@ class StoryListWidget extends ViewModelWidget<HomeViewModel> {
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => StoryViewerScreen(
-                    stories: storyImages,
-                    initialIndex: index,
-                  ),
-                ),
-              );
+              locator<NavigationService>()
+                  .navigateToStoryScreenView(
+                      stories: storyImages,
+                      initialIndex: index,
+                      usernameMap: viewModel.usernameMap,
+                      storyList: storyList);
             },
             child: Padding(
               padding: const EdgeInsets.only(right: 12),
@@ -50,16 +51,26 @@ class StoryListWidget extends ViewModelWidget<HomeViewModel> {
                     padding: const EdgeInsets.all(2),
                     decoration: BoxDecoration(
                         gradient: const LinearGradient(
-                            colors: [Colors.red, Colors.blue, Colors.yellow]),
-                        borderRadius: BorderRadius.circular(100)),
+                            colors: [
+                              Colors.red,
+                              Colors.blue,
+                              Colors.yellow
+                            ]),
+                        borderRadius:
+                            BorderRadius.circular(100)),
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(storyImages[index][0]),
+                      backgroundImage: NetworkImage(
+                          storyImages[index][0]),
                       radius: 25,
                     ),
                   ),
                   Text(
-                    viewModel.usernameMap[storyList[index].userId] ?? '...',
-                    style: Theme.of(context).textTheme.bodySmall,
+                    viewModel.usernameMap[
+                            storyList[index].userId] ??
+                        '...',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall,
                   )
                 ],
               ),
